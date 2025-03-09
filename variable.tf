724eedd006ae9a0b243799da416ac8067ae6524a
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-03-09 14:29:45 -0400

    Create AKS and connected to it

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "labelPrefix" {
+  type        = string
+  default     = "wu000302"
+  description = "Prefix for label."
+}
+
+variable "region" {
+  type        = string
+  default     = "canadacentral"
+  description = "Azure region"
+}
